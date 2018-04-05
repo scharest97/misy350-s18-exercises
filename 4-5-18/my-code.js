91c3b97127dd45c9ceb9c6acc67761b66bf5f112
Samuel Chaarest <scharest@udel.edu>
2018-04-05 10:37:58 -0400

    added js examples

---
+++ 4-5-18/my-code.js
@@ -0,0 +1,13 @@
+blist = ["visit Old Trafford", "visit Yellowstone", "go to Foo Fighters Concert"];
+console.log(blist.length);
+blist.push("get lit");
+
+console.log(blist.length);
+console.log(blist);
+
+/* for loop
+meh
+*/
+for (i=0; i< 4; i++) {
+    console.log(blist[i]);
+}
