72906496c9da3b58bc58abd8600b7ce9273bf1c0
Samuel Chaarest <scharest@udel.edu>
2018-04-19 10:23:34 -0400

    add exercises

---
+++ 4-17-18/python-basics.py
@@ -0,0 +1,9 @@
+# this is my first python code
+greeting = "hello"
+name = "Harry"
+print greeting # this print out a string
+print len(greeting)
+print greeting.upper()
+print greeting[4]
+print greeting + " " + name + "!"
+print "%s %d! how are you doing, today" % (greeting, 5)
