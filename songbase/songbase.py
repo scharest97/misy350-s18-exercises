61244569762fadd17bb63bc0e8ffde0411bd745b
Samuel Chaarest <scharest@udel.edu>
2018-04-26 10:38:37 -0400

    songbase flask

---
+++ songbase/songbase.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "hello Sam"
+
+
+@app.route('/user')
+def user():
+    return "this is the page for users"
+
+if __name__ == '__main__':
+    app.run()
