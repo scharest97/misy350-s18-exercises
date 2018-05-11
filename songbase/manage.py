06f07428590d3a0d4bb98afb504b05d00d813a56
Samuel Chaarest <scharest@udel.edu>
2018-05-11 15:51:30 -0400

    Flash II assignment

--- songbase/manage.py
+++ songbase/manage.py
@@ -1,8 +1,24 @@
 from flask_script import Manager
-from songbase import app
-
+from songbase import app, db, Artist
 
 manager = Manager(app)
+
+
+@manager.command
+def deploy():
+    print "resetting database..."
+    db.drop_all()
+    db.create_all()
+
+    print "inserting initial database..."
+    coldplay= Artist(name="coldplay", about="this is coldplay")
+    m5= Artist(name="Maroon 5", about="this is maroon 5")
+
+    db.session.add(coldplay)
+    db.session.add(m5)
+
+    db.session.commit()
+
 
 
 if __name__ == '__main__':
--- songbase/songbase.py
+++ songbase/songbase.py
@@ -1,5 +1,25 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Artist(db.Model):
+    __tablename__ = 'artists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    about = db.Column(db.Text)
+
+
+@app.route('/artists-all')
+def all_artists():
+    return render_template('all-artists.html')
 
 
 @app.route('/')
@@ -7,18 +27,38 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/form-basics')
-def formbasics():
-    return render_template('form-basics.html')
+@app.route('/artists')
+def artists():
+    artists = Artist.query.all()
+    return render_template('all-artists.html', artists=artists)
 
-@app.route('/form-demo')
-def form_demo():
-    first_name= request.args.get('first_name')
-    return first_name
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method == "GET":
+        # return "hello GET"
+        return render_template('form.html')
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        return render_template('form.html', first_name=first_name)
+
+@app.route('/users/<string:username>')
+def users(username):
+    return render_template('user.html', username=username)
+
+@app.route('/artist/edit/<int:id>')
+def artist_edit(id):
+    artist = Artist.query.filter_by(id=id).first()
+    artist.about= "this is samuel charest"
+    db.session.commit()
+
+    #return render_template('all-artists.html')
+    return "this is artist %d updated" % id
 
 @app.route('/user')
 def user():
     return "this is the page for users"
+
 
 if __name__ == '__main__':
     app.run()
